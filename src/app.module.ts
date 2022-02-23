8a2320206bc05cf4b3fe2327f3b287c8ae90ac9d
saugatdhimal <dhimalsaugat77@gmail.com>
2022-02-23 23:45:05 +0545

    Added auth  and database functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+<<<<<<< HEAD
 
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule, BookmarkModule, PrismaModule],
+=======
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+>>>>>>> caf5a88 (Added auth  and database functionality)
 })
 export class AppModule { }
